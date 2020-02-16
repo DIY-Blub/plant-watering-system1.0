709d69c795b541e4713d68ad65eaf74cdaa9901c
Christian <60984430+DIY-Blub@users.noreply.github.com>
2020-02-16 21:51:55 +0100

    useful for calibration
    Serves to calibrate the sensors. To calculate the percentage value of the soil moisture sensors correctly, it is necessary to know the maximum value of the sensor. In theory, this is 1023, but it can be smaller due to increased resistance (cable length, soldered joints, etc.). Holding a soil moisture sensor in the air, the lowest resistance is determined and the highest value is output. The value must then be defined in the file "waterpi.py". With me the value is 918.

---
+++ sensorkal.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+#coding: utf8
+#sensorkal.py
+import RPi.GPIO as GPIO
+import sys
+import spidev
+from spidev import SpiDev
+import time
+
+class MCP3008:
+    def __init__(self, bus = 0, device = 0):
+        self.bus, self.device = bus, device
+        self.spi = SpiDev()
+        self.open()
+
+    def open(self):
+        self.spi.open(self.bus, self.device)
+
+    def read(self, channel = 0):
+        adc = self.spi.xfer2([1,(8+channel)<<4,0])
+        if 0<=adc[1]<=3:
+           data = ((adc[1]&3)<<8)+adc[2]
+           return data
+        else:
+           return 0
+
+    def close(self):
+        self.spi.close()
+
+# PINS FESTLEGEN
+strom_sensoren = 5
+# GPIO SETUP
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(strom_sensoren, GPIO.OUT)
+
+# FÜHLER ABFRAGEN
+GPIO.output(strom_sensoren, GPIO.HIGH)
+time.sleep(3)
+
+adc = MCP3008()
+value0 = adc.read( channel = 0 ) # Den auszulesenden Channel kannst du natürlich anpassen
+value1 = adc.read( channel = 1 ) # Den auszulesenden Channel kannst du natürlich anpassen
+value2 = adc.read( channel = 2 ) # Den auszulesenden Channel kannst du natürlich anpassen
+value3 = adc.read( channel = 3 ) # Den auszulesenden Channel kannst du natürlich anpassen
+value4 = adc.read( channel = 4 ) # Den auszulesenden Channel kannst du natürlich anpassen
+
+
+time.sleep(3)
+GPIO.output(strom_sensoren, GPIO.LOW)
+time.sleep(0.5)
+
+# Maximalwert eintragen (nie mehr als 1023!)
+max = 1023
+# Ausgabe
+print ("Sensor 1: ",value0," / ",(max - value0) / max * 100)
+print ("Sensor 2: ",value1," / ",(max - value1) / max * 100)
+print ("Sensor 3: ",value2," / ",(max - value2) / max * 100)
+print ("Sensor 4: ",value3," / ",(max - value3) / max * 100)
+print ("Sensor 5: ",value4," / ",(max - value4) / max * 100)
